ff110ea5c1920bc96e7b4f41f06ff96b449d346b
yuvraj <yuvraj@pop-os.localdomain>
2023-04-24 01:12:59 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
-
+// import logo from './logo.svg';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Box1 from "./components/Box1";
+import Teams from "./components/Teams";
+import Box2 from "./components/Box2";
+import Page2 from "./components/Page2";
+import Page3 from "./components/Page3";
+import { ShopContextProvider } from "./context/shop-context";
+// import Aboutus from "./components/Aboutus";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ShopContextProvider>
+      <Navbar />
+      <Routes>
+        <Route
+          path={"/"}
+          element={
+            <>
+              <Box1 />
+              <Teams />
+              <Box2 />
+        {/* <Aboutus/> */}
+            </>
+          }
+        />
+        <Route
+          path={"/page-2"}
+          element={
+            <>
+              <Page2 />
+            </>
+          }
+        />
+        <Route
+          path={"/page-3"}
+          element={
+            <>
+              <Page3 />
+            </>
+          }
+        />
+
+        {/* <Page3/> */}
+      </Routes>
+    </ShopContextProvider>
   );
 }
 
